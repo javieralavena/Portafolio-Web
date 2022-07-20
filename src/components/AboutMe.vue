52388cf7a50c47459e3d9367d213358a210c983b
Javiera <javierabta97@gmail.com>
2022-07-20 12:08:43 -0400

    Arregle el card About me para que sea visible solo desde lg hacia arriba

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,55 @@
+<template>
+
+    <v-card class="card mr-16 mb-5 ms-3 mt-16" outlined>
+        <v-list-item>
+            <v-list-item-content>
+                <h2 class=" mb-4">
+                    Sobre Mí 🤭
+                </h2>
+                <p class="text-justify">
+                    Hola, soy desarrolladora web Front-End Trainee e Ingeniera Civil Industrial, con conocimiento
+                    principalmente en <span>JavaScript</span>, <span>Sass</span> y <span>Vue js</span>.</p>
+                <p class="text-justify">Me considero una persona que tiene facilidad para aprender cosas nuevas y
+                    comprometida con sus responsabilidades, lo que me ha hecho desarrollar la autogestión. Además, el
+                    haber
+                    estudiado una carrera me permitió
+                    desarrollar competencias tales como el trabajo en equipo, el pensamiento lógico y la resolución de
+                    problemas.</p>
+                <p class="text-justify"> Me gusta mucho lo visual, que las cosas sean estéticamente atractivas y
+                    funcionales,
+                    por lo que quiero
+                    dedicarme a desarrollar páginas webs y aplicaciones con estas características, y que le faciliten la
+                    vida a las personas.
+                </p>
+            </v-list-item-content>
+        </v-list-item>
+    </v-card>
+
+</template>
+
+<script>
+export default {
+    name: 'component-AboutMe',
+    // props: {},
+    data: function () {
+        return {}
+    },
+    methods: {
+    },
+}
+</script>
+
+<style scoped>
+.card {
+    border-color: #af9dcc;
+}
+
+p {
+    line-height: 1.5;
+}
+
+span {
+    color: #8f80a7;
+    font-weight: bold;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,31 +9,9 @@
 
       </v-col>
 
-      <v-col cols="12" lg="6" class="col">
+      <v-col cols="12" lg="6">
 
-        <v-card class="card ms-5 mb-5" max-width="100%" outlined>
-          <v-list-item>
-            <v-list-item-content>
-              <h2 class=" mb-4">
-                Sobre Mí 🤭
-              </h2>
-              <p class="text-justify">
-                Hola, soy desarrolladora web Front-End Trainee e Ingeniera Civil Industrial, con conocimiento
-                principalmente en <span>JavaScript</span>, <span>Sass</span> y <span>Vue js</span>.</p>
-              <p class="text-justify">Me considero una persona que tiene facilidad para aprender cosas nuevas y
-                comprometida con sus responsabilidades, lo que me ha hecho desarrollar la autogestión. Además, el haber
-                estudiado una carrera me permitió
-                desarrollar competencias tales como el trabajo en equipo, el pensamiento lógico y la resolución de
-                problemas.</p>
-              <p class="text-justify"> Me gusta mucho lo visual, que las cosas sean estéticamente atractivas y
-                funcionales,
-                por lo que quiero
-                dedicarme a desarrollar páginas webs y aplicaciones con estas características, y que le faciliten la
-                vida a las personas.
-              </p>
-            </v-list-item-content>
-          </v-list-item>
-        </v-card>
+      <AboutMe class="d-none d-lg-flex"></AboutMe>
 
       </v-col>
 
@@ -119,6 +97,7 @@
 <script>
 import CardImg from '@/components/CardImg.vue'
 import RedesContacto from '@/components/RedesContacto.vue'
+import AboutMe from '@/components/AboutMe.vue'
 export default {
   name: 'view-home',
   // props: {},
@@ -137,7 +116,8 @@ export default {
   // watch: {},
   components: {
     CardImg,
-    RedesContacto
+    RedesContacto,
+    AboutMe
   },
   // mixins: [],
   // filters: {},
@@ -150,20 +130,5 @@ export default {
 * {
   box-sizing: border-box;
   font-family: 'Merriweather', serif;
-}
-
-.cardm {
-  margin-top: 60px;
-  border-color: #af9dcc;
-  margin-right: 70px;
-}
-
-p {
-  line-height: 1.5;
-}
-
-span {
-  color: #8f80a7;
-  font-weight: bold;
 }
 </style>
